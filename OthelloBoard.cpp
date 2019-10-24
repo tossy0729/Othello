a2bffcbe437f59be45381474c5cd29cb37ac4d84
池崎利太 <ikezakitoshimasu@h100.178.229.10.1017421.vlan.kuins.net>
2019-10-24 14:16:34 +0900

    add Main.cpp

--- OthelloBoard.cpp
+++ OthelloBoard.cpp
@@ -217,33 +217,4 @@ bool OthelloBoard::check() const //盤面の状態をチェックする
     return fin;
 }
 
-int main(){
-    OthelloBoard O;
-    O.print();
-    bool fin = false;
-    int b = 0;
-    while(!fin){
-        fin = O.check();//盤面のチェック
-        if(!fin){
-            b++;//手番を管理するフラグ
-            cout << b << "手目" << endl;
-            if(b % 2 == 1) cout << "黒の手番です" << endl;
-            else cout << "白の手番です" << endl;
 
-            cout << "２つの数字(x,y)を入力してください" << endl;
-            cout << "終了する場合は10 10と入力してください" << endl;
-            int x,y; cin >> x >> y;
-            if(x == 10 && y == 10) fin = true;//finをtrueにする
-            else if(x>8 || y>8) {
-                b--;
-                cout << "正しい値を入力してください" << endl;
-                continue;
-            }
-            else {
-                O.turn(x-1,y-1,&b);
-                O.print();
-            }
-        }
-    }
-    return 0;
-}
